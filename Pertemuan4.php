df75dd4af555b44d29e3a669ca42131e66a14925
AkhRidwanYus <101609315+AkhRidwanYus@users.noreply.github.com>
2022-03-15 09:18:12 +0800

    Update Pertemuan4.php

--- Pertemuan4.php
+++ Pertemuan4.php
@@ -1,15 +1,14 @@
 <?php
-	echo "Masukkan nilai : ";
-	$nilai = 120
-	if($nilai >= 80) echo "A";
-	else if(69 < $nilai < 80) echo "B";
-	else if(59 < $nilai < 70) echo "C";
-	else if(49 < $nilai < 60) echo "D";
+	$nilai = 120;
+	if ($nilai >= 80) {echo "A";}
+	else if ($nilai >= 70 && $nilai <= 79) {echo "B";}
+	else if ($nilai >= 60 && $nilai <= 69) {echo "C";}
+	else if ($nilai >= 50 && $nilai <= 59) {echo "D";}
 	else echo "E";
 	
-	/*
+	
 	switch($nilai){
-		case T_IS_GREATER_OR_EQUAL(80):
+		case $nilai >=80:
 			echo "A";
 			break;
 		case range(70, 79):
@@ -25,5 +24,4 @@
 			echo "E";
 			break;
 	}
-	*/
 ?>

926a7bd78960afd961c13c9b5e0bfd61625385db
AkhRidwanYus <101609315+AkhRidwanYus@users.noreply.github.com>
2022-03-15 08:55:35 +0800

    Add files via upload

---
+++ Pertemuan4.php
@@ -0,0 +1,29 @@
+<?php
+	echo "Masukkan nilai : ";
+	$nilai = 120
+	if($nilai >= 80) echo "A";
+	else if(69 < $nilai < 80) echo "B";
+	else if(59 < $nilai < 70) echo "C";
+	else if(49 < $nilai < 60) echo "D";
+	else echo "E";
+	
+	/*
+	switch($nilai){
+		case T_IS_GREATER_OR_EQUAL(80):
+			echo "A";
+			break;
+		case range(70, 79):
+			echo "B";
+			break;
+		case range(60, 69):
+			echo "C";
+			break;
+		case range(50, 59):
+			echo "D";
+			break;
+		default:
+			echo "E";
+			break;
+	}
+	*/
+?>
